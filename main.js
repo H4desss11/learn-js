dde382aafc32e032a0e618d55491f3145187a2b6
nikolay.borisov <nikolay.borisov@cargoclix.com>
2023-08-07 15:29:18 +0300

    git initial

---
+++ main.js
@@ -0,0 +1,12 @@
+let monthlySales = Array.of(12, 9, 3);
+let monthlyLabels = Array.of('Oct', 'Nov', 'Dex');
+
+let deptSales = Array.of(12, 9, 3);
+let deotLabes = Array.of('Hiking', 'Running', 'Hunting');
+
+
+
+
+let hiking = { category: 'Hiking' };
+categories.add(hiking);
+alert(categories.has(hiking))
