77efd3681facdba6b9c241b6b97658b1bd6fced9
pavel.marinov <lcs11@abv.bg>
2023-08-11 13:21:21 +0300

    try

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// let monthlySales = Array.of(12, 9, 3);
+// let monthlyLabels = Array.of('Oct', 'Nov', 'Dex');
+
+// let deptSales = Array.of(12, 9, 3);
+// let deotLabes = Array.of('Hiking', 'Running', 'Hunting');
+
+
+
+
+// let hiking = { category: 'Hiking' };
+// categories.add(hiking);
+console.log("main.js is loaded");
+c
